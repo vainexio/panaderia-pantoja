39577a29ef9d198e22a3f185dc1cf833eb84019b
Glitch (project-scseqdjnsjcdbvuisef) <none>
2023-05-25 03:17:55 +0000

    ✌️👭 Checkpoint ./server.js:40274427/60

--- server.js
+++ server.js
@@ -1039,8 +1039,8 @@ client.on('interactionCreate', async inter => {
         //
         let row = new MessageActionRow().addComponents(
           new MessageButton().setCustomId("drop-"+dropMsg.id).setStyle('SECONDARY').setEmoji('📤').setLabel("Release to "+user.user.tag),
-          new MessageButton().setCustomId("showDrop-"+dropMsg.id).setStyle('SECONDARY').setEmoji('❔').setLabel('Show Drop'),
-          new MessageButton().setCustomId("returnLinks-"+dropMsg.id).setStyle('SECONDARY').setEmoji('♻️').setLabel('Return Links')
+          new MessageButton().setCustomId("showDrop-"+dropMsg.id).setStyle('SECONDARY').setEmoji('📋'),
+          new MessageButton().setCustomId("returnLinks-"+dropMsg.id).setStyle('SECONDARY').setEmoji('♻️')
         );
         inter.reply({content: "<:S_exclamation:1093734009005158450> <@"+user.user.id+"> Sending **"+quan.value+"** "+(item ? item.value : 'nitro boost(s)')+".\n<:S_dot:1093733278541951078> Make sure to open your DMs.\n<:S_dot:1093733278541951078> The message may appear as **direct or request** message.", components: [row]})
         //Send auto queue
