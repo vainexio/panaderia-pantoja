af3478ab8fb904148e5d569a6d1a2938764f0695
Glitch (sneaky-verbena-map) <none>
2023-08-13 09:24:00 +0000

    🏰🌛 Checkpoint ./storage/settings_.js:40274427/6 ./functions/others.js:40274427/6

--- storage/settings_.js
+++ storage/settings_.js
@@ -371,7 +371,7 @@ module.exports = {
             children: [
               //
               { name: 'Nitro Basic (MTO)', price: 40, rs: 0 },
-              { name: 'Nitro Boost', price: 110, rs: 125 },
+              { name: 'Nitro Boost', price: 115, rs: 125 },
               //
             ],
           },
