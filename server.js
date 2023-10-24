aa1e5b3d64ba7c144cf992b755d4f5048b49d17e
Glitch (sneaky-verbena-map) <none>
2023-10-24 04:34:29 +0000

    🍔🎄 Checkpoint ./server.js:40274427/53

--- server.js
+++ server.js
@@ -2367,7 +2367,7 @@ app.get('/gcash', async function (req, res) {
       if (transac.amount == data.amount) {
         console.log(transac)
         let cd = await getChannel(transac.channel)
-        await cd.send({embeds: [embed]})
+        await cd.send({content: emojis.check+" Payment Received", embeds: [embed]})
         shop.expected.splice(i,1)
         return;
       }
