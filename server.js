49c9d42c7f736ae635085ae8c818352df91c3af0
Glitch (sneaky-verbena-map) <none>
2024-01-31 13:53:17 +0000

    🏇🎠 Checkpoint ./server.js:40274427/5055

--- server.js
+++ server.js
@@ -321,45 +321,52 @@ client2.on("messageCreate", async (message) => {
     if (message.type === 'REPLY') {
       let msg = await message.channel.messages.fetch(message.reference.messageId)
       if (msg) {
-        let args = getArgs(msg.content)
-        let content = ''
-        let count = 0
-        for (let i in args) {
-          await sleep(100)
-          if (args[i].includes('roblox.com')) {
-            console.log('scan')
-            let auth = {
-              method: 'GET',
-              headers: {
-                'Host': 'www.roblox.com',
-                'Cookie': process.env.robloxCookie,
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-                'Accept-Language': 'en-US,en;q=0.5',
-                'Accept-Encoding': 'gzip, deflate',
-                'Upgrade-Insecure-Requests': '1',
-                'Sec-Fetch-Dest': 'document',
-                'Sec-Fetch-Mode': 'navigate',
-                'Sec-Fetch-Site': 'none',
-                'Sec-Fetch-User': '?1',
-                'Te': 'trailers',
-                'Content-Type': 'application/json'
+        try {
+          let args = getArgs(msg.content)
+          let content = ''
+          let count = 0
+          if (args < 1 || !msg.content.includes('roblox.com')) return message.reply('⚠️ No roblox links was found!')
+          await message.react(emojis.loading)
+          
+          for (let i in args) {
+            await sleep(100)
+            if (args[i].includes('roblox.com')) {
+              console.log('scan')
+              let auth = {
+                method: 'GET',
+                headers: {
+                  'Host': 'www.roblox.com',
+                  'Cookie': process.env.robloxCookie,
+                  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0',
+                  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+                  'Accept-Language': 'en-US,en;q=0.5',
+                  'Accept-Encoding': 'gzip, deflate',
+                  'Upgrade-Insecure-Requests': '1',
+                  'Sec-Fetch-Dest': 'document',
+                  'Sec-Fetch-Mode': 'navigate',
+                  'Sec-Fetch-Site': 'none',
+                  'Sec-Fetch-User': '?1',
+                  'Te': 'trailers',
+                  'Content-Type': 'application/json'
+                }
               }
-            }
-            count++
-            let response = await fetch(args[i].replace(',',''),auth)
+              count++
+              let response = await fetch(args[i].replace(',',''),auth)
             
-            let htmlContent = await response.text()
-            let $ = cheerio.load(htmlContent);
-            let spanElement = $('.text-robux-lg');
-            console.log(spanElement.text())
-            let value = spanElement.text().trim();
-            let raw = Number(value.replace(/,/g,''))
-            let ct = Math.floor(raw*0.7)
-            content +=  '> '+count+'. '+args[i]+'\nPrice: '+emojis.robux+' '+value+'\nYou will receive: **'+emojis.robux+'** '+ct+'\n'
+              let htmlContent = await response.text()
+              let $ = cheerio.load(htmlContent);
+              let spanElement = $('.text-robux-lg');
+              console.log(spanElement.text())
+              let value = spanElement.text().trim();
+              let raw = Number(value.replace(/,/g,''))
+              let ct = Math.floor(raw*0.7)
+              content +=  count+'. '+args[i]+'\nPrice: '+emojis.robux+' '+value+'\nYou will receive: **'+emojis.robux+'** '+ct+'\n\n'
+            }
           }
+          message.channel.send(content)
+        } catch (err) {
+          message.reply(err)
         }
-        message.reply(content)
       }
     }
   }
