f2e4931dfb42897759f8e29dc1772aa507d0f38a
Glitch (printing-services-system) <none>
2025-01-27 14:01:18 +0000

    🌠🌏 Checkpoint ./public/index.html:62737451/60782 ./public/css/login.css:62737451/13347 ./public/scripts/login.js:62737451/1651

---
+++ public/scripts/login.js
@@ -0,0 +1,50 @@
+let selectedUserType = "";
+
+    const doctorButton = document.getElementById("doctorButton");
+    const patientButton = document.getElementById("patientButton");
+    const errorMessageElement = document.getElementById("error-message");
+
+    doctorButton.addEventListener("click", () => {
+      selectedUserType = "doctor";
+      doctorButton.classList.add("selected");
+      patientButton.classList.remove("selected");
+    });
+
+    patientButton.addEventListener("click", () => {
+      selectedUserType = "patient";
+      patientButton.classList.add("selected");
+      doctorButton.classList.remove("selected");
+    });
+
+    document.getElementById("loginForm").addEventListener("submit", async (e) => {
+      e.preventDefault();
+
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+
+      if (!selectedUserType) {
+        errorMessageElement.textContent = "Please select a user type.";
+        errorMessageElement.style.display = "block";
+        return;
+      }
+
+      fetch("/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password, userType: selectedUserType }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.redirect) {
+            window.location.href = data.redirect;
+          } else {
+            errorMessageElement.textContent = data.message;
+            errorMessageElement.style.display = "block";
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    });
