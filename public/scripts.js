61db7c03f5f1374b9a57363fd682aca89e0df153
Glitch (printing-services-system) <none>
2025-01-28 02:29:10 +0000

    👀🚆 Checkpoint ./public/css/main.css:34138681/1039 ./public/scripts.js:34138681/135

--- public/scripts.js
+++ public/scripts.js
@@ -15,10 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
   showSection('dashboard')
     document.querySelectorAll('.nav-link').forEach(link => {
         link.addEventListener('click', function() {
-            // Remove the 'active' class from all nav items
             document.querySelectorAll('.nav-link').forEach(nav => nav.classList.remove('active'));
-            
-            // Add the 'active' class to the clicked nav item
             this.classList.add('active');
         });
     });
