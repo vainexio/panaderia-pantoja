88e30c68dc9c512faa0d663d089faf673a04d3ab
Glitch (printing-services-system) <none>
2025-01-28 13:56:55 +0000

    🎯🕍 Checkpoint ./public/css/main.css:34138681/505 ./public/css/register_patient.css:34138681/11 ./public/doctors.html:34138681/2183 ./public/scripts/particles.js:34138681/1346 ./public/index.html:34138681/2078

---
+++ public/scripts/particles.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", function () {
+  particlesJS('particles-js', {
+    particles: {
+      number: {
+        value: 50,
+        density: {
+          enable: true,
+          value_area: 900
+        }
+      },
+      color: {
+        value: "#ffffff"
+      },
+      shape: {
+        type: "circle"
+      },
+      opacity: {
+        value: 0.5,
+        random: true,
+        anim: {
+          enable: true,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false
+        }
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: {
+          enable: true,
+          speed: 3,
+          size_min: 0.1,
+          sync: false
+        }
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1
+      },
+      move: {
+        enable: true,
+        speed: 1,
+        direction: "none",
+        random: true,
+        straight: false,
+        out_mode: "out",
+        bounce: false
+      }
+    },
+    interactivity: {
+      events: {
+        onhover: {
+          enable: true,
+          mode: "repulse"
+        }
+      },
+      modes: {
+        repulse: {
+          distance: 100,
+          duration: 0.4
+        },
+        push: {
+          particles_nb: 1
+        }
+      }
+    },
+    retina_detect: true
+  });
+});
